d0c6c50f883e7ca94cf3027be941e3e8ee3b7d69
chandana03 <chandanareshma01@gmail.com>
2022-03-30 10:05:36 +0530

    logic

---
+++ factor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace primefactor
+{
+    public class factor
+    {
+        public void primefactors(int n)
+        {
+            
+
+            while(n % 2==0)
+            {
+                n = n / 2;
+            }
+
+            for(int i=3; i<=(n);i++)
+            {
+                while(n%1==0)
+                {
+                    n = n / i;
+
+                }
+            }
+                
+        }
+
+        //private int sqrt(int n)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
